814ed9a553287cdff4b0c1eb3857359d9a3444ce
neo.huang <neo.huang@gigasetdigital.com>
2016-05-16 16:31:05 +0800

    前端-将内容输出给页面

---
+++ 我的文章/前端-将内容输出给页面.php
@@ -0,0 +1,20 @@
+<?php
+class a{
+	public aAction(){
+		$this->setPageBodyContent(''); return;
+	};
+protected function setPageBodyContent($result, $isJson = true){
+        $result = ($isJson == true) ? Mage::helper('core')->jsonEncode($result):$result;
+        $contentType = ($isJson == true) ? 'json':'html';
+        
+        $this->getResponse()
+                ->setHttpResponseCode( 200 )
+                ->setHeader( 'Pragma', 'public', true )
+                ->setHeader( 'Cache-Control', 'must-revalidate, post-check=0, pre-check=0', true )
+                ->setHeader( 'Content-type', 'text/'.$contentType, true )
+                ->setHeader( 'Content-Length', strlen( $result ) )
+                ->setHeader( 'Last-Modified', date( 'r' ) )
+                ->setBody( $result );
+    }
+}
+?>
